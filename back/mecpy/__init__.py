ff5d71e327ad911cbcc5918e4769ee6bc373b1e1
caique325 <caique-magalhaes1@outlook.com>
2023-10-30 07:37:51 -0300

    atualizacao

--- back/mecpy/__init__.py
+++ back/mecpy/__init__.py
@@ -9,6 +9,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'mecpy.sqlite'),
+        UPLOAD_FOLDER = 
     )
 
     if test_config is None:
@@ -31,7 +32,6 @@ def create_app(test_config=None):
     
     from . import db
     db.init_app(app)
-    
     from . import auth
     app.register_blueprint(auth.bp)
 
